8e8bbcd52660e392374a55b97c2333e6cc8ba14e
PrincessDR <PrincessDR>
2017-07-29 12:39:39 +0800

    Comminting my changes princess:

--- MyGitHubProject/MyGitHubProject/Form1.Designer.cs
+++ MyGitHubProject/MyGitHubProject/Form1.Designer.cs
@@ -35,11 +35,11 @@
             // 
             this.label1.AutoSize = true;
             this.label1.Font = new System.Drawing.Font("Monotype Corsiva", 14.25F, System.Drawing.FontStyle.Italic, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.label1.Location = new System.Drawing.Point(71, 94);
+            this.label1.Location = new System.Drawing.Point(41, 52);
             this.label1.Name = "label1";
-            this.label1.Size = new System.Drawing.Size(168, 22);
+            this.label1.Size = new System.Drawing.Size(258, 22);
             this.label1.TabIndex = 0;
-            this.label1.Text = "This is my contribution";
+            this.label1.Text = "This is my contribution -Del Rosario";
             this.label1.Click += new System.EventHandler(this.label1_Click);
             // 
             // Form1
